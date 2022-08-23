ef24dce540955e9f5ddedda92d92053f29b5d47c
Kelvin Brito <britok30@gmail.com>
2022-08-23 11:56:35 -0400

    fix lint

--- components/About.tsx
+++ components/About.tsx
@@ -3,18 +3,18 @@ export const About = () => {
   return (
     <div>
       <h1 className="text-white text-2xl md:text-4xl text-center font-bold mt-14">
-        Hey there, I'm Kelvin Brito! 👨🏽‍💻
+        Hey there, I&apos;m Kelvin Brito! 👨🏽‍💻
       </h1>
 
       <div className="w-[17rem] md:w-96 mx-auto">
         <p className="paragraph my-5">
-          I'm a front-end software engineer specializing in building & designing
+          I&apos;m a front-end software engineer specializing in building & designing
           exceptional, high-quality websites and applications. As I continue
           this life-long journey of self-learning and mastering my craft, I hope
           to be shaped by the challenges and experiences coming my way.
         </p>
         <p className="paragraph mb-5">
-          I thrive at the intersection of tech & art/design. I'm passionate in
+          I thrive at the intersection of tech & art/design. I&apos;m passionate in
           combining my technical knowledge and artistic creativity to build
           beautiful UIs, and continue expanding my knowledge in all things
           front-end.

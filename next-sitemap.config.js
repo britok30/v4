bf9fc70646d013a7a1802e4599b73f9623c15e50
Kelvin Brito <britok30@gmail.com>
2022-09-06 18:32:17 -0400

    sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://britojs.com",
+  generateRobotsTxt: true, 
+  // ...other options
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,8 @@ interface HomeProps {
 const Home = ({ randomPhotoData, downloadData, blurHash }: HomeProps) => {
   const pageTitle = "Kelvin Brito - Front-end Software Engineer";
   const pageDesc =
-    "Kelvin Brito is a front end software engineer specializing in building & designing exceptional, high-quality websites and applications.";
+    "Kelvin Brito is a front-end software engineer specializing in building & designing exceptional, high-quality websites and applications.";
+  const canonicalUrl = "https://britojs.com";
   return (
     <div className="bg-black min-h-screen flex justify-center flex-col">
       <Head>
@@ -27,8 +28,8 @@ const Home = ({ randomPhotoData, downloadData, blurHash }: HomeProps) => {
         <meta content="Kelvin Brito" name="author" />
         <meta name="theme-color" content="#000000" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <meta name="description" content={pageDesc} />
+        <meta name="keywords" content="" />
+        <meta name="description" content={pageDesc} key="desc" />
 
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={pageDesc} key="ogdesc" />
@@ -50,6 +51,8 @@ const Home = ({ randomPhotoData, downloadData, blurHash }: HomeProps) => {
           name="twitter:image"
           content="https://www.britojs.com/portfolio.png"
         />
+        <meta name="robots" content="all" />
+        <link rel="canonical" key="canonical" href={canonicalUrl} />
       </Head>
 
       <div>

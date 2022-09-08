8bfa8baa81e924c9db5ca0af36dc849dff2a83f3
Kelvin Brito <britok30@gmail.com>
2022-09-08 01:41:05 -0400

    add ga id to ephemeral

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,30 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import Script from "next/script";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=G-WPZ4D2X5HE`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+    return (
+        <>
+            <Script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+            />
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-WPZ4D2X5HE');
+          gtag('config', ${process.env.NEXT_PUBLIC_GA_ID});
         `,
-        }}
-      />
-      <Component {...pageProps} />
-    </>
-  );
+                }}
+            />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;

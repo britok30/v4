88aec9eaa00f0638224f498c229a6c8cd8daa3bc
Kelvin Brito <britok30@gmail.com>
2022-09-30 02:08:19 -0400

    sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.kelvinbrito.dev" || "https://.kelvinbrito.dev",
+  siteUrl: "https://www.kelvinbrito.dev",
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   // ...other options
